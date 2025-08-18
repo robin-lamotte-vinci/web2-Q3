531412ded54c7b201be405f85a26717d47459e5f
Robin Lamotte <163841689+robin-lamotte-vinci@users.noreply.github.com>
2025-08-19 00:15:15 +0200

    new: ex1.3

---
+++ exercices/1.3/routes/films.ts
@@ -0,0 +1,113 @@
+import { Router } from "express";
+import { Film, NewFilm } from "../types";
+
+const router = Router();
+
+const defaultFilms: Film[] = [
+  {
+    id: 1,
+    title: "Inception",
+    director: "Christopher Nolan",
+    duration: 148,
+    budget: 160,
+    description:
+      "A thief who steals corporate secrets through dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.",
+    imageUrl: "https://example.com/inception.jpg",
+  },
+  {
+    id: 2,
+    title: "The Matrix",
+    director: "The Wachowskis",
+    duration: 136,
+    budget: 63,
+    imageUrl: "https://example.com/matrix.jpg",
+  },
+  {
+    id: 3,
+    title: "Interstellar",
+    director: "Christopher Nolan",
+    duration: 169,
+    budget: 165,
+    description:
+      "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+  },
+];
+
+router.get("/", (req, res) => {
+  if (!req.query["minimum-duration"]) {
+    return res.json(defaultFilms);
+  }
+
+  const minimumDuration = Number(req.query["minimum-duration"]);
+  const filteredFilms = defaultFilms.filter(
+    (film) => film.duration >= minimumDuration
+  );
+  return res.json(filteredFilms);
+});
+
+router.get("/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const film = defaultFilms.find((film) => film.id === id);
+
+  if (!film) {
+    return res.status(404);
+  }
+
+  return res.json(film);
+});
+
+router.post("/", (req, res) => {
+  const body: unknown = req.body;
+
+  if (
+    !body ||
+    typeof body !== "object" ||
+    !("title" in body) ||
+    !("director" in body) ||
+    !("duration" in body) ||
+    typeof body.title !== "string" ||
+    typeof body.director !== "string" ||
+    typeof body.duration !== "number" ||
+    !body.title.trim() ||
+    !body.director.trim() ||
+    body.duration <= 0
+  ) {
+    return res.sendStatus(400);
+  }
+
+  if (
+    ("budget" in body &&
+      (typeof body.budget !== "number" || body.budget < 0)) ||
+    ("description" in body &&
+      (typeof body.description !== "string" || !body.description.trim())) ||
+    ("imageUrl" in body &&
+      (typeof body.imageUrl !== "string" || !body.imageUrl.trim()))
+  ) {
+    return res.sendStatus(400);
+  }
+
+  const { title, director, duration, budget, description, imageUrl } =
+    body as NewFilm;
+
+  const nextId =
+    defaultFilms.reduce(
+      (maxId, film) => (film.id > maxId ? film.id : maxId),
+      0
+    ) + 1;
+
+  const newFilm: Film = {
+    id: nextId,
+    title,
+    director,
+    duration,
+    budget,
+    description,
+    imageUrl,
+  };
+
+  defaultFilms.push(newFilm);
+
+  return res.json(newFilm);
+});
+
+export default router;
