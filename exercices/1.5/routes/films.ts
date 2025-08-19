0ee27ae6c09b3d045867b5987696170d1f067d22
Robin Lamotte <163841689+robin-lamotte-vinci@users.noreply.github.com>
2025-08-19 02:16:14 +0200

    new: ex1.5

---
+++ exercices/1.5/routes/films.ts
@@ -0,0 +1,123 @@
+import { Router } from "express";
+import { Film, NewFilm } from "../types";
+import { isNewFilm } from "../utils/type-guards";
+
+const router = Router();
+
+const defaultFilms: Film[] = [
+  {
+    id: 1,
+    title: "Inception",
+    director: "Christopher Nolan",
+    duration: 148,
+    budget: 160,
+    description:
+      "A thief who steals corporate secrets through dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.",
+    imageUrl: "https://example.com/inception.jpg",
+  },
+  {
+    id: 2,
+    title: "The Matrix",
+    director: "The Wachowskis",
+    duration: 136,
+    budget: 63,
+    imageUrl: "https://example.com/matrix.jpg",
+  },
+  {
+    id: 3,
+    title: "Interstellar",
+    director: "Christopher Nolan",
+    duration: 169,
+    budget: 165,
+    description:
+      "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+  },
+];
+
+router.get("/", (req, res) => {
+  const allowedQueries = ["minimum-duration"];
+  const extra = Object.keys(req.query).filter(
+    (k) => !allowedQueries.includes(k)
+  );
+  if (extra.length > 0) {
+    return res.sendStatus(400);
+  }
+
+  if (!req.query["minimum-duration"]) {
+    return res.json(defaultFilms);
+  }
+
+  const minimumDuration = Number(req.query["minimum-duration"]);
+  if (Number.isNaN(minimumDuration) || minimumDuration <= 0) {
+    return res.sendStatus(400);
+  }
+
+  const filteredFilms = defaultFilms.filter(
+    (film) => film.duration >= minimumDuration
+  );
+  return res.json(filteredFilms);
+});
+
+router.get("/:id", (req, res) => {
+  const id = Number(req.params.id);
+
+  if (isNaN(id)) {
+    return res.sendStatus(400);
+  }
+
+  const film = defaultFilms.find((film) => film.id === id);
+
+  if (!film) {
+    return res.status(404);
+  }
+
+  return res.json(film);
+});
+
+router.post("/", (req, res) => {
+  const body: unknown = req.body;
+
+  if (!isNewFilm(body)) {
+    return res.sendStatus(400);
+  }
+
+  const expectedKeys = [
+    "title",
+    "director",
+    "duration",
+    "budget",
+    "description",
+    "imageUrl",
+  ];
+  const bodyKeys = Object.keys(body);
+  const extraKeys = bodyKeys.filter((key) => !expectedKeys.includes(key));
+  if (extraKeys.length > 0) {
+    return res.sendStatus(400);
+  }
+
+  const newFilm: NewFilm = body;
+
+  const existingFilm = defaultFilms.find(
+    (film) =>
+      film.title.toLowerCase() === newFilm.title.toLowerCase() &&
+      film.director.toLowerCase() === newFilm.director.toLowerCase()
+  );
+  
+  if (existingFilm) {
+    return res.sendStatus(409);
+  }
+
+  const nextId =
+    defaultFilms.reduce(
+      (maxId, film) => (film.id > maxId ? film.id : maxId),
+      0
+    ) + 1;
+
+  const addedFilm: Film = { id: nextId, ...newFilm };
+
+  defaultFilms.push(addedFilm);
+
+  return res.json(addedFilm);
+});
+
+export default router;
