a193139ed56698d00e48ade686b94b33846d256d
Robin Lamotte <163841689+robin-lamotte-vinci@users.noreply.github.com>
2025-08-24 02:03:56 +0200

    new:ex2.5-events

--- exercices/2.5/src/components/App/App.tsx
+++ exercices/2.5/src/components/App/App.tsx
@@ -15,7 +15,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <ClickCounter title="StateFull components are usefull !!" message="You are a master in the art of clicking !">
+      <ClickCounter title="StateFull components are usefull !!">
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
--- exercices/2.5/src/components/ClickCounter/index.tsx
+++ exercices/2.5/src/components/ClickCounter/index.tsx
@@ -1,23 +1,55 @@
 import { useState } from "react";
+import "./ClickCounter.css";
 
 interface ClickCounterProps {
-    title: string;
-    message: string;
-    children: React.ReactNode;
+  title: string;
+  messageCount10?: string;
+  messageOnMouseEnter?: string;
+  children: React.ReactNode;
 }
 
-const ClickCounter = ({title, message, children} : ClickCounterProps) => {
+const ClickCounter = ({
+  title,
+  messageCount10 = "You are a master in the art of clicking !",
+  messageOnMouseEnter = "Please click on me now!",
+  children,
+}: ClickCounterProps) => {
   const [count, setCount] = useState(0);
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleOnMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleOnMouseLeave = () => {
+    setIsHovered(false);
+  };
 
   return (
     <div className="card">
-        <h2>{title}</h2>
-      <button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-      </button>
-      {count >= 10 && <p> {message}</p>}
-      {children}
+      <h2>{title}</h2>
+      <div className="counter-button">
+        {isHovered ? (
+          <div>
+            {messageOnMouseEnter}
+            <br />
+          </div>
+        ) : (
+          <div>
+            <br />
+          </div>
+        )}
 
+        <button
+          onClick={() => setCount((count) => count + 1)}
+          onMouseEnter={handleOnMouseEnter}
+          onMouseLeave={handleOnMouseLeave}
+        >
+          count is {count}
+        </button>
+      </div>
+      {count >= 10 && <p> {messageCount10}</p>}
+      {children}
     </div>
   );
 };
